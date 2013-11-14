1a5501e2a7fa616b8b8f5b47efea34cfe9bb05a6
Paulo Barcelos <paulo@azucrina.org>
2013-11-14 08:22:32 +0100

    Parsing port number

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(process.env.PORT || 5000);
+var io = require('socket.io').listen(parseInt(process.env.PORT) || 5000);
 
 function generateUUID(a){
 	return a? (a ^ Math.random()* 16 >> a/4).toString(16): ([1e7] + -1e3 + -4e3 + -8e3 +-1e11 ).replace( /[018]/g, generateUUID );
